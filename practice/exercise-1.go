d0adfe247fb04b5ed2a53af8f7721811197c7fdd
sumanth797 <sumanth619icon@gmail.com>
2021-05-03 01:28:41 +0530

    Done

--- practice/exercise-1.go
+++ practice/exercise-1.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+const (
+	A = iota
+	B = iota
+	C
+	D = iota + 5
+	E
+)
+const (
+	A1 = iota
+	B1 = iota
+	C1
+)
+
 func main() {
 	x, y, z := 42, "JamesBond", true
 	fmt.Println(x, y, z)
@@ -9,5 +22,7 @@ func main() {
 		a int    = 2
 		b string = "how"
 	)
+	fmt.Println(A, B, C, D, E)
+	fmt.Println(A1, B1, C1)
 
 }
